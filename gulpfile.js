7281de557188ffd9ef130e1b2fda47c75700bae1
nmccready <nemtcan@gmail.com>
2021-10-18 13:12:24 -0400

    chore: remove unecessary gulp

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var testGlob = './test/**/*.spec.js';
-
-exports.default = function () {
-  return gulp.src(testGlob, {read: false})
-    .pipe(mocha());
-}
